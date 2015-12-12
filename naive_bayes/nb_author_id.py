0dcf56cf2f65d105b39a7dd86228e47856ccec30
Aaron <foss.aaron@gmail.com>
2015-12-12 10:25:57 -0600

    feat: add code for gaussian naive bayes test

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -11,7 +11,6 @@
 """
     
 import sys
-from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
@@ -27,7 +26,33 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+# classify with Naive Bayes and return accuracy on test set
+def classifyNB(features_train, lables_train, features_test, labels_test):
+    from sklearn.naive_bayes import GaussianNB
+    from sklearn.metrics import accuracy_score
+    from time import time    
+    
+    clf = GaussianNB()
+
+    train_time = time()
+    clf.fit(features_train, labels_train)
+    train_time = time() - train_time    
+    
+    pred_time = time()
+    pred = clf.predict(features_test)
+    pred_time = time() - pred_time    
+    
+    accuracy = accuracy_score(labels_test, pred)
+    
+    return accuracy, train_time, pred_time
+
 
 #########################################################
 
-
+if __name__ == "__main__":
+    
+    accuracy, train_time, pred_time = classifyNB(features_train, labels_train, features_test, labels_test)
+    
+    print "accuracy:", round(accuracy, 3)
+    print "training time:", round(train_time, 3), "s"
+    print "prediction time:", round(pred_time, 3), "s"
