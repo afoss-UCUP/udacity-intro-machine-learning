2e7fe45fc04f417257b81d5647430c635522a1ca
Aaron <foss.aaron@gmail.com>
2015-12-15 11:15:20 -0600

    feat: add function to clean data of worst 10% outliers

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -10,11 +10,19 @@ def outlierCleaner(predictions, ages, net_worths):
         Return a list of tuples named cleaned_data where 
         each tuple is of the form (age, net_worth, error).
     """
+    import numpy as np
+    # get squared residuals    
+    error = (net_worths - predictions)
     
-    cleaned_data = []
-
+    # order the index of squared residuals (descending)
+    ordered_ind = sorted(range(len(error**2)), key=lambda k: (error**2)[k], reverse = True)
+    
+    # find the point to begin keeping observations 
+    ind_beg = int(.1 * len(ages))
+    
+    cleaned_data = np.concatenate((ages, net_worths, error), axis = 1)[ordered_ind[(ind_beg):],:]
+    
     ### your code goes here
-
     
     return cleaned_data
 
